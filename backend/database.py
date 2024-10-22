1210628e2db20fbb44679f8bf3a02328f81c7b13
kjalba <albakevinjulian@gmail.com>
2024-10-22 12:59:39 -0500

    style(db): updated db connection to use .env vars instead of writing out the url

--- backend/database.py
+++ backend/database.py
@@ -2,9 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = "postgresql://user:password@localhost/songalyze"
+from core.config import settings
 
-engine = create_engine(URL_DATABASE)
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+print("Database URL is ",SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
