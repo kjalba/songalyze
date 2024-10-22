1c000b633d43dd58f7df2bd7b2551fdc09d582ff
kjalba <albakevinjulian@gmail.com>
2024-10-21 20:06:52 -0500

    style(env): Added .env vars and .gitignore

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,54 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends
+from typing import Annotated
+from sqlalchemy.orm import Session
+from pydantic import BaseModel
+from database import SessionLocal, engine
+import models
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    'http://localhost:3000',
+]
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+)
+
+class SongBase(BaseModel):
+    title: str
+    artist: str
+    chord_progression: str
+    mood: str
+    genre: str
+    bpm: int
+    lyrics: str
+
+class SongModel(SongBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+models.Base.metadata.create_all(bind=engine)
+
+@app.post("/songs/", response_model=SongModel)
+async def create_song(song: SongBase, db: Session = db_dependency):
+    db_song = models.Song(**song.model_dump())
+    db.add(db_song)
+    db.commit()
+    db.refresh(db_song)
+    return db_song
